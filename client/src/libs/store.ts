ad6478f658e7321837fe3a38a3cfbe0c8ab54885
Drugged-Monkey <drugged_monkey@outlook.com>
2021-10-24 22:52:36 +0300

    [feature] add some persistance to authentication

--- client/src/libs/store.ts
+++ client/src/libs/store.ts
@@ -6,6 +6,9 @@ import { ActionType, IAction, IApplicationState, IHeaderLevelItem } from "./inte
 import { commonReducer, headerReducer, mmReducer, authReducer } from './reducers';
 import { loggerMiddleware, changeSeasonMiddleware, changeTownMiddleware, changeTownAndSeasonMiddleware } from './middlewares';
 import { loadTownsAsMenuItemsThunkActionCreator } from './thunkActions';
+import { getAuth } from "@firebase/auth";
+import { firebaseApp } from "./firebase";
+import { exchangeGoogleTokens, getUser } from "./repositories";
 
 const rootReducer = combineReducers<IApplicationState, IAction>({
     common: commonReducer,
@@ -25,10 +28,31 @@ const composedEnhancer = composeWithDevTools(
 export const store = createStore(rootReducer, composedEnhancer);
 
 export const fetchInitialData = async (): Promise<void> => {
-    store.dispatch({ 
+    store.dispatch({
         type: ActionType.ADDMANY_LEVEL1,
-        payload: [{ name: "Home", link: "/" }, { name: "About", link: "/about" }, {name: "Admin", link: "/admin" }] as IHeaderLevelItem[]
+        payload: [{ name: "Home", link: "/" }, { name: "About", link: "/about" }, { name: "Admin", link: "/admin" }] as IHeaderLevelItem[]
     });
 
     (store.dispatch as ThunkDispatch<IApplicationState, {}, IAction>)(loadTownsAsMenuItemsThunkActionCreator());
+
+    const auth = getAuth(firebaseApp);
+
+    console.log(auth);
+
+    auth.onAuthStateChanged((user: any) => {
+        if (!!user && user.uid) {
+            getUser(user.uid)
+                .then((user) => {
+                    if (!!user) {
+                        store.dispatch({ type: ActionType.AUTH_SIGNIN, payload: user });
+                        return Promise.resolve();
+                    } else {
+                        return Promise.reject("Something went wrong during authentication user.");
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    });
 };
